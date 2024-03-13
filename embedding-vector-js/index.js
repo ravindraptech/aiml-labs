c6e3b60d355be598be4a2e98a40ff0ec812d4442
Ravindra Pandya <124175453+ravindraptech@users.noreply.github.com>
2024-03-13 17:35:21 +0530

    Update index.js

--- embedding-vector-js/index.js
+++ embedding-vector-js/index.js
@@ -1,3 +1,11 @@
 import openai from './config.js';
 
 /** Create embeddings for input text */
+async function main() {
+  const embedding = await openai.embeddings.create({
+    model: "text-embedding-ada-002",
+    input: "The quick brown fox jumped over the lazy dog",
+  });
+  console.log(embedding);
+}
+main();
