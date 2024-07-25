bdf77e5f4d54df56a750fffa73d814205e2670e6
ravindraptech <ravindrap.tech@gmail.com>
2024-07-25 12:04:06 +0530

    chabot demo customize

--- vercel-next-chatgpt-bot/app/page.tsx
+++ vercel-next-chatgpt-bot/app/page.tsx
@@ -5,8 +5,9 @@ export const runtime = 'edge'
 export default function Home() {
   return (
     <main className="container mx-auto">
-      <h1 className="text-center my-8 text-2xl">YouTubeGPT</h1>
+      <h1 className="text-center my-8 text-2xl">ChatBot demo by Ravindra P.</h1>
       <Chat />
+      <div><br/><br/><p>* Using Next.js, ChatGPT API deployed on Vercel (ref Ethan)</p></div>
     </main>
   )
 }
